2e3cd832178c968db3b9226afcb01f3daebdc45b
Tally <113806118+thenatally@users.noreply.github.com>
2025-05-07 17:43:27 -0400

    Revert "add reconnect"
    This reverts commit a3f79cff106ebcd41afc2fad6769b4e253320409.

--- client.ts
+++ client.ts
@@ -1,66 +1,32 @@
+
 import net from 'net';
 import WebSocket from 'ws';
 const WS_SERVER_URL = 'wss://mcbridge.tally.gay';
-
 const tcpServer = net.createServer((tcpSocket) => {
   console.log('Client connected');
-  let reconnectToken: string | null = null;
-  let tcpBuffer: Buffer[] = [];
-  let ws: WebSocket | null = null;
+  const ws = new WebSocket(WS_SERVER_URL);
 
-  const connectWebSocket = () => {
-    ws = new WebSocket(WS_SERVER_URL);
-
-    ws.on('open', () => {
-      console.log('WebSocket connection established');
-      if (reconnectToken) {
-        ws?.send(JSON.stringify({ type: 'reconnect', token: reconnectToken }));
-      }
-
-      tcpSocket.on('data', (data) => {
-        if (ws?.readyState === WebSocket.OPEN) {
-          ws.send(data);
-        } else {
-          tcpBuffer.push(data);
-        }
-      });
-
-      ws?.on('message', (msg) => {
-        const message = JSON.parse(msg.toString());
-        if (message.type === 'uuid') {
-          reconnectToken = message.token;
-        } else if (message.type === 'data') {
-          tcpSocket.write(Buffer.from(message.data));
-        }
-      });
-
-      while (tcpBuffer.length > 0) {
-        ws?.send(tcpBuffer.shift()!);
-      }
+  ws.on('open', () => {
+    console.log('WebSocket connection established');
+    tcpSocket.on('data', (data) => {
+      ws.send(data);
     });
-
-    ws.on('close', () => {
-      console.log('WebSocket disconnected, attempting to reconnect...');
-      setTimeout(connectWebSocket, 10);
+    ws.on('message', (msg) => {
+      tcpSocket.write(msg as Buffer);
     });
-
-    ws.on('error', (err) => {
-      console.error('WebSocket error:', err);
-      ws?.close();
-    });
-  };
-
-  connectWebSocket();
+  });
 
   const cleanup = () => {
     tcpSocket.destroy();
-    ws?.close();
+    ws.close();
   };
 
   tcpSocket.on('close', cleanup);
   tcpSocket.on('error', cleanup);
+  ws.on('close', cleanup);
+  ws.on('error', cleanup);
 });
 
 tcpServer.listen(9999, () => {
-  console.log('TLink v2');
+  console.log('Client listening on port 9999');
 });
--- server.ts
+++ server.ts
@@ -1,62 +1,32 @@
+
 import net from 'net';
 import { WebSocketServer } from 'ws';
-import { v4 as uuidv4 } from 'uuid';
 
 const MINECRAFT_SERVER_HOST = 'localhost';
 const MINECRAFT_SERVER_PORT = 25565;
 
 const wss = new WebSocketServer({ port: 8080 });
-const clientMap = new Map<string, { ws: any; tcpSocket: net.Socket; buffer: Buffer[] }>();
 
 wss.on('connection', (ws) => {
   console.log('WebSocket client connected');
-  const clientId = uuidv4();
-  let tcpSocket: net.Socket | null = null;
-  const tcpBuffer: Buffer[] = [];
-
-  const setupTcpSocket = () => {
-    tcpSocket = net.connect(MINECRAFT_SERVER_PORT, MINECRAFT_SERVER_HOST, () => {
-      console.log('Connected to Minecraft server');
-    });
-
-    tcpSocket.on('data', (data) => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.send(JSON.stringify({ type: 'data', data: data.toString('base64') }));
-      } else {
-        tcpBuffer.push(data);
-      }
-    });
-
-    tcpSocket.on('close', cleanup);
-    tcpSocket.on('error', cleanup);
-  };
-
-  ws.on('message', (msg) => {
-    const message = JSON.parse(msg.toString());
-    if (message.type === 'reconnect' && clientMap.has(message.token)) {
-      console.log('Reconnecting WebSocket client');
-      const client = clientMap.get(message.token)!;
-      client.ws = ws;
-      client.buffer.forEach((data) => ws.send(JSON.stringify({ type: 'data', data: data.toString('base64') })));
-      client.buffer.length = 0;
-    } else if (message.type === 'data') {
-      tcpSocket?.write(Buffer.from(message.data, 'base64'));
-    }
+  const tcpSocket = net.connect(MINECRAFT_SERVER_PORT, MINECRAFT_SERVER_HOST, () => {
+    console.log('Connected to Minecraft server');
   });
-
-  ws.send(JSON.stringify({ type: 'uuid', token: clientId }));
-  clientMap.set(clientId, { ws, tcpSocket: tcpSocket!, buffer: tcpBuffer });
-
+  ws.on('message', (msg) => {
+    tcpSocket.write(msg as Buffer);
+  });
+  tcpSocket.on('data', (data) => {
+    ws.send(data);
+  });
   const cleanup = () => {
-    tcpSocket?.destroy();
-    clientMap.delete(clientId);
+    tcpSocket.destroy();
     ws.close();
   };
 
   ws.on('close', cleanup);
   ws.on('error', cleanup);
-
-  setupTcpSocket();
+  tcpSocket.on('close', cleanup);
+  tcpSocket.on('error', cleanup);
 });
 
 console.log('Server listening on port 8080');
